76e370e447edbd26ccec8f5fa80e53b5a3e5fb46
Zach <zmglindro@up.edu.ph>
2024-01-12 23:22:40 +0800

    Add 2024 projections for future testing

--- program.py
+++ program.py
@@ -3,21 +3,28 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 import data
 
-data.get(2015, 2023)
-print('Loading data...')
-pairs = data.load(2015, 2023)
+# TODO: add PA as ballast
+# Returns the regression model
+def start():
+    data.get(2015, 2023)
+    print('Loading data...')
+    pairs = data.load(2015, 2023)
 
-def startLinear():
     print('\nTraining model...')
 
     # Split data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(pairs[['Barrel%_prev', 'wRC+_prev', 'K%_prev', 'BB%_prev', 'Age_prev']], pairs['wRC+_curr'], test_size=0.3, random_state=0)
-    
+
     # Train model
     regression = linear_model.LinearRegression()
     regression.fit(X_train, y_train)
 
     print(f'Score: {regression.score(X_test, y_test)}')
+
+    return regression
+
+def tui():
+    model = start()
 
     # Predict wRC+ based on user input
     while True:
@@ -40,6 +47,26 @@ def startLinear():
             continue
 
         input_df = pd.DataFrame({'Barrel%_prev': barrel, 'wRC+_prev': wrc, 'K%_prev': k, 'BB%_prev': bb, 'Age_prev': age}, index=[0])
-        print(f'Predicted wRC+: {round(regression.predict(input_df)[0])}')
+        print(f'Predicted wRC+: {round(model.predict(input_df)[0])}')
 
-startLinear()
+# Project wRC+ for 2024 for each player
+def project2024():
+    model = start()
+    d23 = pd.read_csv(f'./data/batting_2023.tsv', sep='|')
+
+    projections = pd.DataFrame()
+
+    for player in d23['Name']:
+        barrel = d23[d23['Name'] == player]['Barrel%'].values[0]
+        wrc = d23[d23['Name'] == player]['wRC+'].values[0]
+        k = d23[d23['Name'] == player]['K%'].values[0]
+        bb = d23[d23['Name'] == player]['BB%'].values[0]
+        age = d23[d23['Name'] == player]['Age'].values[0]
+
+        input_df = pd.DataFrame({'Barrel%_prev': barrel, 'wRC+_prev': wrc, 'K%_prev': k, 'BB%_prev': bb, 'Age_prev': age}, index=[0])
+        print(f'{player}: {round(model.predict(input_df)[0])}')
+        projections = pd.concat([projections, pd.DataFrame({'Name': player, 'wRC+': round(model.predict(input_df)[0])}, index=[0])], ignore_index=True)
+    
+    projections.to_csv('2024_projections.tsv', sep='|', index=False)
+
+tui()
