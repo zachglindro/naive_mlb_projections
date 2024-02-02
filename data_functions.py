d85cd757eaac357d529680e156605aa4369a6176
Zach <zmglindro@up.edu.ph>
2024-02-02 13:25:46 +0800

    Apply pylint fixes

--- data_functions.py
+++ data_functions.py
@@ -1,14 +1,16 @@
+"""Module for getting batter data using pybaseball and loading it into a pandas DataFrame"""
+import os
 import pybaseball as pb
 import pandas as pd
-import os
 from tqdm import tqdm
 
 DEFAULT_START_YEAR = 2015
 DEFAULT_END_YEAR = 2023
 MINIMUM_PA = 300
 
-# Get batter data for the model
-def get(start_year=DEFAULT_START_YEAR, end_year=DEFAULT_END_YEAR, exclude_2020=True, force_update=False):
+def get(start_year=DEFAULT_START_YEAR, end_year=DEFAULT_END_YEAR,
+        exclude_2020=True, force_update=False):
+    """Gets batter data per season"""
     if not os.path.exists('data'):
         os.mkdir('data')
 
@@ -18,15 +20,15 @@ def get(start_year=DEFAULT_START_YEAR, end_year=DEFAULT_END_YEAR, exclude_2020=T
     for season in tqdm(range(start_year, end_year+1), desc='Getting data'):
         if season == 2020 and exclude_2020:
             continue
-        
+
         if os.path.exists(f'data/batting_{season}.csv') and not force_update:
             continue
 
         data = pb.batting_stats(season, qual=MINIMUM_PA, ind=1)
         data.to_csv(f'data/batting_{season}.csv')
 
-# Load batter data for the model
 def load(start_year=DEFAULT_START_YEAR, end_year=DEFAULT_END_YEAR):
+    """Loads batter data into a pandas DataFrame and merges it with the previous season's data"""
     data = pd.DataFrame()
 
     all_data = {}
